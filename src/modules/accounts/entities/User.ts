54f1bfa16cf07b2c116cef0d2689476c4d1dacd8
carlos <carlosaugustodelimavieira@protonmail.com>
2022-07-05 22:06:56 -0300

    feat: :sparkles: create a entities user

---
+++ src/modules/accounts/entities/User.ts
@@ -0,0 +1,35 @@
+import { Entity, Column, PrimaryColumn, CreateDateColumn } from "typeorm";
+import { v4 as uuidV4 } from "uuid";
+
+@Entity()
+
+class User {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  driver_license: string;
+
+  @Column()
+  isAdmin: boolean;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  constructor() {
+    if(!this.id){
+      this.id = uuidV4();
+    }
+  }
+};
+
+export { User }; 
